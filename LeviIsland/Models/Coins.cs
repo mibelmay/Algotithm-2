bc2036fc0154a2b626aba5b404992038268c1ebb
Stacy <zemcovannastasia11@gmail.com>
2023-10-18 21:06:26 +0500

    added Coins class

---
+++ LeviIsland/Models/Coins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeviIsland.Models
+{
+    public class Coins
+    {
+        public int CountWays(int[] coins, int amount)
+        {
+            if (amount == 0)
+            {
+                return 1;
+            }
+            if (amount < 0)
+            {
+                return 0;
+            }
+            int ways = 0;
+            foreach (int coin in coins)
+            {
+                ways += CountWays(coins, amount - coin);
+            }
+            return ways;
+        }
+    }
+}
